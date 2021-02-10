19e5f7a17533121973bc3814c3b1dcdf046f4f27
Richard Chu <richardchu10@gmail.com>
2021-02-10 15:21:52 -0500

    Add Tailwind CSS, add tsconfig baseUrl

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: { '@typescript-eslint/explicit-module-boundary-types': 'off' },
 };
---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
