23eddc044a9783c5c55fe3b2a7fecdc0c2596ec9
Richard Chu <richardchu10@gmail.com>
2021-02-10 14:40:02 -0500

    Set up Typescript, remove starter code

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Atomic</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Atomic</h1>
+      </main>
+    </div>
+  );
+}
