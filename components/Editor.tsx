f3de44c3f63a05b4fc4479df4a6ed74023e19a5f
Richard Chu <richardchu10@gmail.com>
2021-02-12 15:28:03 -0500

    Add formatting shortcuts

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,19 +1,107 @@
-import React, { useMemo, useState } from 'react';
-import { createEditor, Node } from 'slate';
-import { Editable, Slate, withReact } from 'slate-react';
+import React, { useCallback, useMemo, useState } from 'react';
+import { createEditor, Editor as SlateEditor, Node } from 'slate';
+import {
+  Editable,
+  RenderElementProps,
+  RenderLeafProps,
+  Slate,
+  withReact,
+} from 'slate-react';
+import { withHistory } from 'slate-history';
+import isHotkey from 'is-hotkey';
+
+const HOTKEYS: Record<string, string> = {
+  'mod+b': 'bold',
+  'mod+i': 'italic',
+  'mod+u': 'underline',
+  'mod+`': 'code',
+};
 
 export default function Editor() {
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
   const [value, setValue] = useState<Array<Node>>([
     {
       type: 'paragraph',
-      children: [{ text: 'Start typing here...' }],
+      children: [{ text: '' }],
     },
   ]);
 
   return (
     <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
-      <Editable className="w-full h-screen p-8" />
+      <Editable
+        className="w-full h-screen p-8"
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        placeholder="Start typing here…"
+        spellCheck
+        autoFocus
+        onKeyDown={(event) => {
+          for (const hotkey in HOTKEYS) {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            if (isHotkey(hotkey, event as any)) {
+              event.preventDefault();
+              const mark = HOTKEYS[hotkey];
+              toggleMark(editor, mark);
+            }
+          }
+        }}
+      />
     </Slate>
   );
 }
+
+const isMarkActive = (editor: SlateEditor, format: string) => {
+  const marks = SlateEditor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
+
+const toggleMark = (editor: SlateEditor, format: string) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    SlateEditor.removeMark(editor, format);
+  } else {
+    SlateEditor.addMark(editor, format, true);
+  }
+};
+
+const Element = ({ attributes, children, element }: RenderElementProps) => {
+  switch (element.type) {
+    case 'block-quote':
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case 'bulleted-list':
+      return <ul {...attributes}>{children}</ul>;
+    case 'heading-one':
+      return <h1 {...attributes}>{children}</h1>;
+    case 'heading-two':
+      return <h2 {...attributes}>{children}</h2>;
+    case 'list-item':
+      return <li {...attributes}>{children}</li>;
+    case 'numbered-list':
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }: RenderLeafProps) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
