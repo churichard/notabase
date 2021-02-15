2cb8a5814e3874b90f82463da951cb0ef1d2f247
Richard Chu <richardchu10@gmail.com>
2021-02-15 17:31:58 -0500

    Update sidebar formatting

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col flex-none w-64 h-screen py-4 bg-gray-50 md:border-r md:border-gray-100 md:max-w-2xs">
+    <div className="flex flex-col flex-none w-64 h-screen bg-gray-50 md:border-r md:border-gray-100 md:max-w-2xs">
       <Link href="/app">
-        <a className="w-full px-8 py-1 text-lg font-medium text-gray-800 hover:bg-gray-200">
+        <a className="w-full px-8 py-3 font-medium text-gray-800 hover:bg-gray-200 active:bg-gray-300">
           Atomic
         </a>
       </Link>
       <Link href="/app">
-        <a className="w-full px-8 py-1 mt-4 text-gray-800 hover:bg-gray-200">
+        <a className="w-full px-8 py-1 mt-2 text-gray-800 hover:bg-gray-200 active:bg-gray-300">
           Test
         </a>
       </Link>
