72a0bbb54e1c02f0e38a7032c038a14bae1dfc13
Richard Chu <richardchu10@gmail.com>
2021-03-11 18:00:44 -0500

    Automatically create link when a url is written

--- editor/plugins/withLinks.ts
+++ editor/plugins/withLinks.ts
@@ -1,3 +1,4 @@
+import { Editor as SlateEditor, Range, Transforms } from 'slate';
 import { ReactEditor } from 'slate-react';
 import isUrl from 'is-url';
 import { wrapLink } from 'editor/formatting';
@@ -10,8 +11,36 @@ const withLinks = (editor: ReactEditor) => {
   };
 
   editor.insertText = (text) => {
+    const { selection } = editor;
+
     if (text && isUrl(text)) {
       wrapLink(editor, text);
+    } else if (text === ' ' && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = SlateEditor.above(editor, {
+        match: (n) => SlateEditor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      const start = SlateEditor.start(editor, path);
+      const range = { anchor, focus: start };
+      const beforeText = SlateEditor.string(editor, range);
+      const beforeTextArr = beforeText.split(' ');
+      const lastSegment = beforeTextArr[beforeTextArr.length - 1];
+
+      // If the last text segment is a URL, convert it into a link
+      if (lastSegment && isUrl(lastSegment)) {
+        const lastSegmentRange = {
+          anchor,
+          focus: {
+            path: anchor.path,
+            offset: anchor.offset - lastSegment.length,
+          },
+        };
+        Transforms.select(editor, lastSegmentRange);
+        wrapLink(editor, lastSegment);
+      }
+
+      insertText(text);
     } else {
       insertText(text);
     }
