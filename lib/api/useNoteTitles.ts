9af83f26fba873c057e5cd6d4ac053c88e502e6b
Richard Chu <richardchu10@gmail.com>
2021-04-09 15:44:37 -0400

    Remove user id from useNoteTitles

--- lib/api/useNoteTitles.ts
+++ lib/api/useNoteTitles.ts
@@ -1,27 +1,17 @@
 import useSWR, { SWRConfiguration } from 'swr';
 import supabase from 'lib/supabase';
 import { Note } from 'types/supabase';
-import { useAuth } from 'utils/useAuth';
 
 export const NOTE_TITLES_KEY = 'api/noteTitles';
 
 export default function useNoteTitles(options?: SWRConfiguration) {
-  const { user } = useAuth();
-  return useSWR<Array<Note>>(
-    NOTE_TITLES_KEY,
-    () => getNoteTitles(user?.id ?? ''),
-    options
-  );
+  return useSWR<Array<Note>>(NOTE_TITLES_KEY, getNoteTitles, options);
 }
 
-const getNoteTitles = async (userId: string) => {
-  if (!userId) {
-    return [];
-  }
+const getNoteTitles = async () => {
   const { data } = await supabase
     .from<Note>('notes')
     .select('id, title')
-    .eq('user_id', userId)
     .order('title');
   return data ?? [];
 };
