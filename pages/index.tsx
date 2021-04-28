cfff536978702ec7537086c5eb0408b71c948306
Richard Chu <richardchu10@gmail.com>
2021-04-28 17:14:50 -0400

    Add github link, adjust spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,31 @@ export default function Home() {
         </title>
       </Head>
       <div className="flex flex-col h-screen bg-gray-50">
-        <div className="px-6 mx-auto max-w-8xl">
-          <div className="container flex justify-between py-6">
+        <div className="container px-6 max-w-7xl">
+          <div className="flex items-center justify-between py-6">
             <Link href="/">
               <a className="text-xl">Notabase</a>
             </Link>
-            <Link href="/login">
-              <a className="text-lg">Sign in</a>
-            </Link>
+            <div className="space-x-6">
+              <a
+                href="https://github.com/churichard/notabase"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <Link href="/login">
+                <a>Sign in</a>
+              </Link>
+            </div>
           </div>
-          <div className="container py-16 text-center md:py-32">
+          <div className="pt-16 text-center md:pt-32">
             <h1 className="text-4xl font-medium md:text-6xl">
               A wiki for your brain.
             </h1>
-            <h2 className="mt-4 text-2xl text-gray-700 md:mt-8 md:text-4xl">
+            <p className="pt-4 text-2xl text-gray-700 md:pt-8 md:text-3xl">
               Notabase is a personal knowledge base for non-linear thinking.
-            </h2>
+            </p>
             <Link href="/signup">
               <a className="inline-block mt-4 font-medium md:mt-8 btn">
                 Start your knowledge base
