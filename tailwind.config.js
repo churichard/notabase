4c53777e3bfca7a7262fe6cc820fc40b972fcc20
Richard Chu <richardchu10@gmail.com>
2021-04-30 16:22:11 -0400

    Enable Tailwind JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -24,10 +25,10 @@ module.exports = {
         'rgb(15 15 15 / 10%) 0px 3px 6px, rgb(15 15 15 / 20%) 0px 9px 24px',
     },
   },
-  variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-    display: ['responsive', 'group-hover'],
-    extend: {},
-  },
+  // variants: {
+  //   backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  //   display: ['responsive', 'group-hover'],
+  //   extend: {},
+  // },
   plugins: [require('@tailwindcss/forms')],
 };
