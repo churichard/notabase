730af6ac5098edbb8e9e9d5bdc80876ce7b8181a
Richard Chu <richardchu10@gmail.com>
2021-05-06 16:56:15 -0400

    Add basic favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <ProvideAuth>
         <Component {...pageProps} />
