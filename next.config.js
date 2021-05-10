fa936d49233ed6c8049cc7bee4a881ea4bd9af46
Richard Chu <richardchu10@gmail.com>
2021-05-10 12:38:53 -0400

    Update tabler icons, headless ui, remove next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-// TODO: remove this once https://github.com/tabler/tabler-icons/issues/126 is fixed
-const withTabler = require('next-transpile-modules')(['@tabler/icons']);
 
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-  return withTabler({
+  return {
     future: {
       webpack5: true,
     },
     env: {
       BASE_URL: isDev ? 'http://localhost:3000' : 'https://notabase.io',
     },
-  });
+  };
 };
