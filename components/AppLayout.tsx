9792914cd7c2392ab2cf3372ffb1bce234fa765b
Richard Chu <richardchu10@gmail.com>
2021-05-21 13:33:36 -0400

    Add keyboard shortcut for find or create modal

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useMemo } from 'react';
 import { useAtom } from 'jotai';
+import isHotkey from 'is-hotkey';
 import useNoteTitles from 'lib/api/useNoteTitles';
 import { isFindOrCreateModalOpen } from 'lib/state';
 import { Note } from 'types/supabase';
@@ -16,6 +17,30 @@ export default function AppLayout(props: Props) {
   const { children, initialNotes, className } = props;
   const { data: notes } = useNoteTitles({ initialData: initialNotes });
   const [isModalOpen, setIsModalOpen] = useAtom(isFindOrCreateModalOpen);
+
+  const hotkeys = useMemo(
+    () => [
+      {
+        hotkey: 'mod+p',
+        callback: () => setIsModalOpen((isOpen) => !isOpen),
+      },
+    ],
+    [setIsModalOpen]
+  );
+
+  useEffect(() => {
+    const handleKeyboardShortcuts = (event: KeyboardEvent) => {
+      for (const { hotkey, callback } of hotkeys) {
+        if (isHotkey(hotkey, event)) {
+          event.preventDefault();
+          callback();
+        }
+      }
+    };
+    document.addEventListener('keydown', handleKeyboardShortcuts);
+    return () =>
+      document.removeEventListener('keydown', handleKeyboardShortcuts);
+  }, [hotkeys]);
 
   return (
     <div className={`flex h-screen ${className}`}>
