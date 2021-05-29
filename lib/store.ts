69cb32dc36be118b7e51fb7f6d6741833d22c632
Richard Chu <richardchu10@gmail.com>
2021-05-28 22:24:43 -0400

    Use immer to update zustand store

--- lib/store.ts
+++ lib/store.ts
@@ -1,7 +1,17 @@
 import create from 'zustand';
 import createVanilla from 'zustand/vanilla';
+import produce from 'immer';
+import type { State, StateCreator } from 'zustand';
+import type { Draft } from 'immer';
 import type { Note } from 'types/supabase';
 import type { NoteUpdate } from './api/updateNote';
+
+const immer =
+  <T extends State>(
+    config: StateCreator<T, (fn: (draft: Draft<T>) => void) => void>
+  ): StateCreator<T> =>
+  (set, get, api) =>
+    config((fn) => set(produce<T>(fn)), get, api);
 
 export type Store = {
   notes: Note[];
@@ -13,75 +23,87 @@ export type Store = {
   setOpenNoteIds: (openNoteIds: Note['id'][], index?: number) => void;
 };
 
-export const store = createVanilla<Store>((set, get) => ({
-  /**
-   * An array of saved notes
-   */
-  notes: [],
-  /**
-   * Sets the notes
-   */
-  setNotes: (value: Note[] | ((value: Note[]) => Note[])) => {
-    if (typeof value === 'function') {
-      set((state) => ({ notes: value(state.notes) }));
-    } else {
-      set({ notes: value });
-    }
-  },
-  /**
-   * If the note id exists, then update the note. Otherwise, insert it
-   */
-  upsertNote: (note: Note) => {
-    const notes = get().notes;
-    const index = notes.findIndex((n) => n.id === note.id);
+export const store = createVanilla<Store>(
+  immer((set) => ({
+    /**
+     * An array of saved notes
+     */
+    notes: [],
+    /**
+     * Sets the notes
+     */
+    setNotes: (value: Note[] | ((value: Note[]) => Note[])) => {
+      if (typeof value === 'function') {
+        set((state) => {
+          state.notes = value(state.notes);
+        });
+      } else {
+        set((state) => {
+          state.notes = value;
+        });
+      }
+    },
+    /**
+     * If the note id exists, then update the note. Otherwise, insert it
+     */
+    upsertNote: (note: Note) => {
+      set((state) => {
+        const index = state.notes.findIndex((n) => n.id === note.id);
 
-    if (index >= 0) {
-      notes[index] = { ...notes[index], ...note };
-    } else {
-      notes.push(note);
-    }
-    set({ notes });
-  },
-  /**
-   * Update the given note
-   */
-  updateNote: (note: NoteUpdate) => {
-    const notes = get().notes;
-    const index = notes.findIndex((n) => n.id === note.id);
+        if (index >= 0) {
+          state.notes[index] = { ...state.notes[index], ...note };
+        } else {
+          state.notes.push(note);
+        }
+      });
+    },
+    /**
+     * Update the given note
+     */
+    updateNote: (note: NoteUpdate) => {
+      set((state) => {
+        const index = state.notes.findIndex((n) => n.id === note.id);
 
-    if (index >= 0) {
-      notes[index] = { ...notes[index], ...note };
-      set({ notes });
-    }
-  },
-  /**
-   * Delete the note with the given noteId
-   */
-  deleteNote: (noteId: string) => {
-    const notes = get().notes;
-    const index = notes.findIndex((note) => note.id === noteId);
-    if (index >= 0) {
-      notes.splice(index, 1);
-      set({ notes });
-    }
-  },
-  /**
-   * The note ids that have their content visible, including the main note and the stacked notes
-   */
-  openNoteIds: [],
-  /**
-   * Replaces the open note ids at the given index (0 by default)
-   */
-  setOpenNoteIds: (newOpenNoteIds: Note['id'][], index?: number) => {
-    if (!index) {
-      set({ openNoteIds: newOpenNoteIds });
-      return;
-    }
-    // Replace the notes after the current note with the new note
-    const openNoteIds = get().openNoteIds;
-    openNoteIds.splice(index, openNoteIds.length - index, ...newOpenNoteIds);
-    set({ openNoteIds });
-  },
-}));
+        if (index >= 0) {
+          state.notes[index] = { ...state.notes[index], ...note };
+        }
+      });
+    },
+    /**
+     * Delete the note with the given noteId
+     */
+    deleteNote: (noteId: string) => {
+      set((state) => {
+        const index = state.notes.findIndex((note) => note.id === noteId);
+        if (index >= 0) {
+          state.notes.splice(index, 1);
+        }
+      });
+    },
+    /**
+     * The note ids that have their content visible, including the main note and the stacked notes
+     */
+    openNoteIds: [],
+    /**
+     * Replaces the open note ids at the given index (0 by default)
+     */
+    setOpenNoteIds: (newOpenNoteIds: Note['id'][], index?: number) => {
+      if (!index) {
+        set((state) => {
+          state.openNoteIds = newOpenNoteIds;
+        });
+        return;
+      }
+      // Replace the notes after the current note with the new note
+      set((state) => {
+        state.openNoteIds.splice(
+          index,
+          state.openNoteIds.length - index,
+          ...newOpenNoteIds
+        );
+      });
+    },
+  }))
+);
 
 export const useStore = create(store);
