ae95d2095945c498d2a2fd2fce2db28bf24a044a
Richard Chu <richardchu10@gmail.com>
2021-06-02 16:30:02 -0400

    Add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-  return {
+  return withBundleAnalyzer({
     future: {
       webpack5: true,
     },
     env: {
       BASE_URL: isDev ? 'http://localhost:3000' : 'https://notabase.io',
     },
-  };
+  });
 };
