016e217e76dc35e94f4ce17c221effda30597c41
Richard Chu <richardchu10@gmail.com>
2021-06-02 14:45:20 -0400

    Remove unnecessary local cache update in updateNote

--- lib/api/updateNote.ts
+++ lib/api/updateNote.ts
@@ -1,5 +1,4 @@
 import type { PickPartial } from 'types/utils';
-import { store } from 'lib/store';
 import supabase from 'lib/supabase';
 import type { Note } from 'types/supabase';
 
@@ -11,11 +10,6 @@ export default async function updateNote(note: NoteUpdate) {
     .update(note)
     .eq('id', note.id)
     .single();
-
-  if (!response.error) {
-    // Update note title in sidebar and backlinks in other open notes
-    store.getState().updateNote(note);
-  }
 
   return response;
 }
