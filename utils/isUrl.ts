b52c2740d751624aeba06a5bba5aaf862723a15a
Richard Chu <richardchu10@gmail.com>
2021-06-30 15:15:03 -0400

    Update isUrl to be faster

--- utils/isUrl.ts
+++ utils/isUrl.ts
@@ -1,13 +1,12 @@
-// Adapted from https://stackoverflow.com/a/5717133
+// Adapted from https://stackoverflow.com/a/43467144
 export default function isUrl(str: string) {
-  const pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  ); // fragment locator
-  return !!pattern.test(str);
+  let url;
+
+  try {
+    url = new URL(str);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
 }
