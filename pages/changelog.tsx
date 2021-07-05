92cc2246d109abd8afc3dc5b278c558171aa87fa
Richard Chu <richardchu10@gmail.com>
2021-07-05 13:57:22 -0400

    Add description to changelog page

--- pages/changelog.tsx
+++ pages/changelog.tsx
@@ -13,6 +13,20 @@ export default function Changelog() {
         <h1 className="text-4xl font-medium text-center md:text-5xl">
           Changelog
         </h1>
+        <p className="pt-6">
+          This page lists when major features and bug fixes are added to
+          Notabase. Note that Notabase is constantly being updated and this page
+          may not be comprehensive. For a full list of technical changes, go to{' '}
+          <a
+            className="link"
+            href="https://github.com/churichard/notabase/commits/main"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Notabase&apos;s public GitHub
+          </a>
+          .
+        </p>
         <ChangelogBlock
           title="July 2, 2021"
           changes={['Add thematic break / horizontal rule']}
