f3ae68f71723a937b758981a968ccca51e40f629
Richard Chu <richardchu10@gmail.com>
2021-07-21 13:16:42 -0400

    Add sentry

--- next.config.js
+++ next.config.js
@@ -1,20 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
+const { withSentryConfig } = require('@sentry/nextjs');
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-  return withBundleAnalyzer({
-    i18n: {
-      locales: ['en-US'],
-      defaultLocale: 'en-US',
-    },
-    env: {
-      BASE_URL: isDev ? 'http://localhost:3000' : 'https://notabase.io',
-    },
-  });
+  return withSentryConfig(
+    withBundleAnalyzer({
+      i18n: {
+        locales: ['en-US'],
+        defaultLocale: 'en-US',
+      },
+      env: {
+        BASE_URL: isDev ? 'http://localhost:3000' : 'https://notabase.io',
+      },
+    }),
+    SentryWebpackPluginOptions
+  );
 };
