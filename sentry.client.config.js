264ee77f196cc5ef7ec8818eacea1f4151d952a7
Richard Chu <richardchu10@gmail.com>
2021-07-27 16:16:40 -0400

    Only enable sentry in production

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,6 +10,7 @@ Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0,
+  enabled: process.env.NODE_ENV === 'production',
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
