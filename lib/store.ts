78da8ed5c0f0141c01924f8e5ffcc53e8da478af
Richard Chu <richardchu10@gmail.com>
2021-07-27 14:12:21 -0400

    Persist user settings

--- lib/store.ts
+++ lib/store.ts
@@ -1,6 +1,8 @@
 import create, { State, StateCreator } from 'zustand';
 import createVanilla from 'zustand/vanilla';
+import { persist, StateStorage } from 'zustand/middleware';
 import produce, { Draft } from 'immer';
+import localforage from 'localforage';
 import type { Note } from 'types/supabase';
 import { Sort, UserSettings } from 'constants/userSettings';
 import type { NoteUpdate } from './api/updateNote';
@@ -14,6 +16,15 @@ const immer =
   ): StateCreator<T> =>
   (set, get, api) =>
     config((fn) => set(produce<T>(fn)), get, api);
+
+const storage: StateStorage = {
+  getItem: async (name: string): Promise<string | null> => {
+    return await localforage.getItem(name);
+  },
+  setItem: async (name: string, value: string): Promise<void> => {
+    await localforage.setItem(name, value);
+  },
+};
 
 export type Notes = Record<Note['id'], Note>;
 
@@ -36,112 +47,120 @@ export type Store = {
 };
 
 export const store = createVanilla<Store>(
-  immer((set) => ({
-    /**
-     * An array of saved notes
-     */
-    notes: {},
-    /**
-     * Sets the notes
-     */
-    setNotes: (value: Notes | ((value: Notes) => Notes)) => {
-      if (typeof value === 'function') {
-        set((state) => {
-          state.notes = value(state.notes);
-        });
-      } else {
-        set((state) => {
-          state.notes = value;
-        });
-      }
-    },
-    /**
-     * If the note id exists, then update the note. Otherwise, insert it
-     */
-    upsertNote: (note: Note) => {
-      set((state) => {
-        if (state.notes[note.id]) {
-          state.notes[note.id] = { ...state.notes[note.id], ...note };
+  persist(
+    immer((set) => ({
+      /**
+       * An array of saved notes
+       */
+      notes: {},
+      /**
+       * Sets the notes
+       */
+      setNotes: (value: Notes | ((value: Notes) => Notes)) => {
+        if (typeof value === 'function') {
+          set((state) => {
+            state.notes = value(state.notes);
+          });
         } else {
-          state.notes[note.id] = note;
+          set((state) => {
+            state.notes = value;
+          });
         }
-      });
-    },
-    /**
-     * Update the given note
-     */
-    updateNote: (note: NoteUpdate) => {
-      set((state) => {
-        if (state.notes[note.id]) {
-          state.notes[note.id] = { ...state.notes[note.id], ...note };
-        }
-      });
-    },
-    /**
-     * Delete the note with the given noteId
-     */
-    deleteNote: (noteId: string) => {
-      set((state) => {
-        delete state.notes[noteId];
-      });
-    },
-    /**
-     * The notes that have their content visible, including the main note and the stacked notes
-     */
-    openNoteIds: [],
-    /**
-     * Replaces the open notes at the given index (0 by default)
-     */
-    setOpenNoteIds: (newOpenNoteIds: string[], index?: number) => {
-      if (!index) {
+      },
+      /**
+       * If the note id exists, then update the note. Otherwise, insert it
+       */
+      upsertNote: (note: Note) => {
         set((state) => {
-          state.openNoteIds = newOpenNoteIds;
-        });
-        return;
-      }
-      // Replace the notes after the current note with the new note
-      set((state) => {
-        state.openNoteIds.splice(
-          index,
-          state.openNoteIds.length - index,
-          ...newOpenNoteIds
-        );
-      });
-    },
-    isSidebarOpen: true,
-    setIsSidebarOpen: (value: boolean | ((value: boolean) => boolean)) => {
-      if (typeof value === 'function') {
-        set((state) => {
-          state.isSidebarOpen = value(state.isSidebarOpen);
-        });
-      } else {
-        set((state) => {
-          state.isSidebarOpen = value;
-        });
-      }
-    },
-    isPageStackingOn: true,
-    setIsPageStackingOn: (value: boolean | ((value: boolean) => boolean)) => {
-      if (typeof value === 'function') {
-        set((state) => {
-          state.isPageStackingOn = value(state.isPageStackingOn);
-        });
-      } else {
-        set((state) => {
-          state.isPageStackingOn = value;
-        });
-      }
-    },
-    userSettings: {
-      noteSort: Sort.TitleAscending,
-    },
-    updateUserSettingByKey:
-      (key: keyof UserSettings) => (value: UserSettings[typeof key]) => {
-        set((state) => {
-          state.userSettings[key] = value;
+          if (state.notes[note.id]) {
+            state.notes[note.id] = { ...state.notes[note.id], ...note };
+          } else {
+            state.notes[note.id] = note;
+          }
         });
       },
-  }))
+      /**
+       * Update the given note
+       */
+      updateNote: (note: NoteUpdate) => {
+        set((state) => {
+          if (state.notes[note.id]) {
+            state.notes[note.id] = { ...state.notes[note.id], ...note };
+          }
+        });
+      },
+      /**
+       * Delete the note with the given noteId
+       */
+      deleteNote: (noteId: string) => {
+        set((state) => {
+          delete state.notes[noteId];
+        });
+      },
+      /**
+       * The notes that have their content visible, including the main note and the stacked notes
+       */
+      openNoteIds: [],
+      /**
+       * Replaces the open notes at the given index (0 by default)
+       */
+      setOpenNoteIds: (newOpenNoteIds: string[], index?: number) => {
+        if (!index) {
+          set((state) => {
+            state.openNoteIds = newOpenNoteIds;
+          });
+          return;
+        }
+        // Replace the notes after the current note with the new note
+        set((state) => {
+          state.openNoteIds.splice(
+            index,
+            state.openNoteIds.length - index,
+            ...newOpenNoteIds
+          );
+        });
+      },
+      isSidebarOpen: true,
+      setIsSidebarOpen: (value: boolean | ((value: boolean) => boolean)) => {
+        if (typeof value === 'function') {
+          set((state) => {
+            state.isSidebarOpen = value(state.isSidebarOpen);
+          });
+        } else {
+          set((state) => {
+            state.isSidebarOpen = value;
+          });
+        }
+      },
+      isPageStackingOn: true,
+      setIsPageStackingOn: (value: boolean | ((value: boolean) => boolean)) => {
+        if (typeof value === 'function') {
+          set((state) => {
+            state.isPageStackingOn = value(state.isPageStackingOn);
+          });
+        } else {
+          set((state) => {
+            state.isPageStackingOn = value;
+          });
+        }
+      },
+      userSettings: {
+        noteSort: Sort.TitleAscending,
+      },
+      updateUserSettingByKey:
+        (key: keyof UserSettings) => (value: UserSettings[typeof key]) => {
+          set((state) => {
+            state.userSettings[key] = value;
+          });
+        },
+    })),
+    {
+      name: 'notabase-storage',
+      version: 1,
+      getStorage: () => storage,
+      whitelist: ['userSettings'],
+    }
+  )
 );
 
 export const useStore = create(store);
