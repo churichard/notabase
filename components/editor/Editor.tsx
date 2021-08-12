bf79d914d7d37b5e651a1b103c73a927516643e7
Richard Chu <richardchu10@gmail.com>
2021-08-12 17:45:02 -0400

    Remove custom auto scrolling on type

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -4,7 +4,6 @@ import {
   useMemo,
   useState,
   KeyboardEvent,
-  MouseEvent,
   useEffect,
 } from 'react';
 import {
@@ -213,48 +212,6 @@ export default function Editor(props: Props) {
     [hotkeys]
   );
 
-  const onSelect = useCallback(
-    (event: KeyboardEvent<HTMLDivElement> | MouseEvent<HTMLDivElement>) => {
-      /**
-       * Add auto scrolling on type
-       * Adapted from https://github.com/ianstormtaylor/slate/issues/3750
-       */
-      if (
-        !editor.selection ||
-        event.nativeEvent.type === 'keyup' ||
-        event.nativeEvent.type === 'mousedown' ||
-        event.nativeEvent.type === 'mouseup' ||
-        event.nativeEvent.altKey ||
-        event.nativeEvent.metaKey ||
-        event.nativeEvent.ctrlKey
-      ) {
-        // Don't auto scroll
-        return;
-      }
-      try {
-        /**
-         * Need a try/catch because sometimes you get an error like:
-         *
-         * Error: Cannot resolve a DOM node from Slate node: {"type":"p","children":[{"text":"","by":-1,"at":-1}]}
-         */
-        const domPoint = ReactEditor.toDOMPoint(editor, editor.selection.focus);
-        const node = domPoint[0];
-        if (!node) return;
-        const element = node.parentElement;
-        if (!element) return;
-        // Only scroll element into view if it is not visible
-        if (!isElementInViewport(element)) {
-          element.scrollIntoView({ block: 'nearest' });
-        }
-      } catch (e) {
-        /**
-         * Empty catch. Do nothing if there is an error.
-         */
-      }
-    },
-    [editor]
-  );
-
   // If highlightedPath is defined, highlight the path
   useEffect(() => {
     if (!highlightedPath) {
@@ -312,7 +269,6 @@ export default function Editor(props: Props) {
         renderLeaf={renderLeaf}
         placeholder="Start typing here…"
         onKeyDown={onKeyDown}
-        onSelect={onSelect}
         onMouseDown={() => setToolbarCanBeVisible(false)}
         onMouseUp={() => setTimeout(() => setToolbarCanBeVisible(true), 100)}
         spellCheck
@@ -320,14 +276,3 @@ export default function Editor(props: Props) {
     </Slate>
   );
 }
-
-const isElementInViewport = (el: Element) => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
