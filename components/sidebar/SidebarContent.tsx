26c460965b5970d49cb05142041362f9f40b8e42
Richard Chu <richardchu10@gmail.com>
2021-09-05 12:56:44 -0400

    Refactor sidebar tabs

--- components/sidebar/SidebarContent.tsx
+++ components/sidebar/SidebarContent.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, useState } from 'react';
-import { IconFile, IconSearch } from '@tabler/icons';
+import { IconFile, IconSearch, TablerIcon } from '@tabler/icons';
 import Tooltip from 'components/Tooltip';
 import SidebarNotes from './SidebarNotes';
 import SidebarSearch from './SidebarSearch';
@@ -47,43 +47,44 @@ const Tabs = (props: TabsProps) => {
   return (
     <div className="flex">
       <Tooltip content="Notes List">
-        <button
-          className={`flex justify-center flex-1 py-1.5 px-6 rounded-t hover:bg-gray-100 active:bg-gray-200 dark:hover:bg-gray-700 dark:active:bg-gray-600 dark:border-gray-700 ${
-            activeTab === SidebarTab.Notes
-              ? 'bg-gray-50 border-t border-r dark:bg-gray-800'
-              : 'border-b'
-          }`}
-          onClick={() => setActiveTab(SidebarTab.Notes)}
-        >
-          <IconFile
-            size={20}
-            className={
-              activeTab === SidebarTab.Notes
-                ? 'text-gray-800 dark:text-gray-300'
-                : 'text-gray-500'
-            }
-          />
-        </button>
+        <Tab
+          isActive={activeTab === SidebarTab.Notes}
+          setActive={() => setActiveTab(SidebarTab.Notes)}
+          Icon={IconFile}
+        />
       </Tooltip>
       <Tooltip content="Search">
-        <button
-          className={`flex justify-center flex-1 py-1.5 px-6 rounded-t hover:bg-gray-100 active:bg-gray-200 dark:hover:bg-gray-700 dark:active:bg-gray-600 dark:border-gray-700 ${
-            activeTab === SidebarTab.Search
-              ? 'bg-gray-50 border-t border-l dark:bg-gray-800'
-              : 'border-b'
-          }`}
-          onClick={() => setActiveTab(SidebarTab.Search)}
-        >
-          <IconSearch
-            size={20}
-            className={
-              activeTab === SidebarTab.Search
-                ? 'text-gray-800 dark:text-gray-300'
-                : 'text-gray-500'
-            }
-          />
-        </button>
+        <Tab
+          isActive={activeTab === SidebarTab.Search}
+          setActive={() => setActiveTab(SidebarTab.Search)}
+          Icon={IconSearch}
+        />
       </Tooltip>
     </div>
+  );
+};
+
+type TabProps = {
+  isActive: boolean;
+  setActive: () => void;
+  Icon: TablerIcon;
+};
+
+const Tab = (props: TabProps) => {
+  const { isActive, setActive, Icon } = props;
+  return (
+    <button
+      className={`flex justify-center flex-1 py-1.5 px-6 rounded-t hover:bg-gray-100 active:bg-gray-200 dark:hover:bg-gray-700 dark:active:bg-gray-600 dark:border-gray-700 ${
+        isActive ? 'bg-gray-50 border-t border-r dark:bg-gray-800' : 'border-b'
+      }`}
+      onClick={setActive}
+    >
+      <Icon
+        size={20}
+        className={
+          isActive ? 'text-gray-800 dark:text-gray-300' : 'text-gray-500'
+        }
+      />
+    </button>
   );
 };
