bb88bd9732f6174cc31abe50bc2d81256501abcf
Richard Chu <richardchu10@gmail.com>
2021-10-04 16:15:11 -0400

    Add faq question

--- components/PricingFaq.tsx
+++ components/PricingFaq.tsx
@@ -1,3 +1,5 @@
+import { MAX_NUM_OF_BASIC_NOTES } from 'constants/pricing';
+
 type Props = {
   className?: string;
 };
@@ -68,6 +70,21 @@ export default function PricingFaq(props: Props) {
                 full price. You&apos;ll be locked in at this lower price as long
                 as your subscription remains active. Prices will go up soon, so
                 act fast.
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 className="text-lg font-medium text-gray-900 dark:text-gray-100">
+              What happens if I reach {MAX_NUM_OF_BASIC_NOTES} notes on the
+              Basic plan?
+            </h3>
+            <div className="mt-2">
+              <p className="text-gray-600 dark:text-gray-300">
+                Once you reach {MAX_NUM_OF_BASIC_NOTES} notes on the Basic plan,
+                you won&apos;t be able to create any more notes and will be
+                asked to upgrade. You will still be able to browse your notes,
+                export them, and use all functionality that doesn&apos;t involve
+                creating notes.
               </p>
             </div>
           </div>
