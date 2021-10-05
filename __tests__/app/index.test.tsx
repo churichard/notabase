1c701334c6c602af09b0dee5a587c09e40f5e0f9
Richard Chu <richardchu10@gmail.com>
2021-10-05 15:28:58 -0400

    Add testing library and simple app test

---
+++ __tests__/app/index.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, act } from '@testing-library/react';
+import { store } from 'lib/store';
+import AppHome from 'pages/app/index';
+
+describe('AppHome', () => {
+  it('renders empty text state', () => {
+    render(<AppHome />);
+
+    const emptyText = screen.getByText(
+      'Get started by clicking “Find or Create Note” in the sidebar'
+    );
+    expect(emptyText).toBeInTheDocument();
+  });
+
+  it('does not render open sidebar button if the sidebar is open', () => {
+    render(<AppHome />);
+
+    act(() => {
+      store.getState().setIsSidebarOpen(true);
+    });
+
+    const openSidebarButton = screen.queryByRole('button', {
+      name: 'Open sidebar',
+    });
+    expect(openSidebarButton).toBeNull();
+  });
+
+  it('renders open sidebar button if the sidebar is closed', () => {
+    render(<AppHome />);
+
+    act(() => {
+      store.getState().setIsSidebarOpen(false);
+    });
+
+    const openSidebarButton = screen.getByRole('button', {
+      name: 'Open sidebar',
+    });
+    expect(openSidebarButton).toBeInTheDocument();
+  });
+});
