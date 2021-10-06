3ddef1fc0e720453c71143417f63e36d5797b8b8
Richard Chu <churichard@users.noreply.github.com>
2021-10-06 13:42:21 -0700

    Fix blank graph view when a note links to a nonexistent note (#258)

--- __tests__/app/graph.test.tsx
+++ __tests__/app/graph.test.tsx
@@ -1,11 +1,73 @@
 import { render, screen, act } from '@testing-library/react';
-import { store } from 'lib/store';
+import { Notes, store } from 'lib/store';
 import Graph from 'pages/app/graph';
 import notes from '__fixtures__/notes';
+import { ElementType } from 'types/slate';
 
 describe('Graph', () => {
   it('renders graph canvas', () => {
     act(() => {
+      store.getState().setNotes(notes);
+    });
+
+    render(<Graph />);
+
+    const canvas = screen.getByTestId('graph-canvas');
+    expect(canvas).toBeInTheDocument();
+  });
+
+  it('renders graph canvas if a note links to a nonexistent note', () => {
+    act(() => {
+      const notes: Notes = {};
+
+      notes['note-1'] = {
+        id: 'note-1',
+        title: `Note`,
+        content: [
+          {
+            id: 'node-1',
+            type: ElementType.Paragraph,
+            children: [
+              {
+                id: 'node-2',
+                type: ElementType.Paragraph,
+                children: [
+                  {
+                    text: 'This is a paragraph',
+                  },
+                ],
+              },
+              {
+                id: 'node-3',
+                type: ElementType.Paragraph,
+                children: [
+                  {
+                    text: 'Linking to ',
+                  },
+                  {
+                    id: 'node-4',
+                    type: ElementType.NoteLink,
+                    noteId: 'note-2',
+                    children: [
+                      {
+                        text: 'new note',
+                      },
+                    ],
+                    noteTitle: 'new note',
+                  },
+                  {
+                    text: '. ',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        user_id: '1',
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      };
+
       store.getState().setNotes(notes);
     });
 
