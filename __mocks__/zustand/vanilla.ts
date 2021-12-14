f7ede73716289125b07ca961b4a0264afd4e77ef
Richard Chu <richardchu10@gmail.com>
2021-12-14 15:39:43 -0500

    Add zustand mock types

--- __mocks__/zustand/vanilla.ts
+++ __mocks__/zustand/vanilla.ts
@@ -1,11 +1,11 @@
-import actualCreate from 'zustand/vanilla';
+import actualCreate, { StateCreator, State } from 'zustand/vanilla';
 import { act } from '@testing-library/react';
 
 // a variable to hold reset functions for all stores declared in the app
 const storeResetFns = new Set<() => void>();
 
 // when creating a store, we get its initial state, create a reset function and add it in the set
-const create: typeof actualCreate = (createState) => {
+const create = <T extends State>(createState: StateCreator<T>) => {
   const store = actualCreate(createState);
   const initialState = store.getState();
   storeResetFns.add(() => store.setState(initialState, true));
