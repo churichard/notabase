ed5014d73ea2aa8d35e0caafa57b096023be595a
Richard Chu <richardchu10@gmail.com>
2022-04-02 19:28:58 -0400

    Rename purge to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class',
   theme: {
     container: {
