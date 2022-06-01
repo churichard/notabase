fd83069aa13caea2f659c4aa5b3e01be0e7e86bd
Michael Fritzsch <15315366+mfridge@users.noreply.github.com>
2022-06-01 19:34:41 +0000

    Disable sentry if not configured

--- next.config.js
+++ next.config.js
@@ -16,6 +16,9 @@ const SentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
+  // Disable sentry if not configured.
+  dryRun: process.env.NEXT_PUBLIC_SENTRY_DSN === '',
+
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
