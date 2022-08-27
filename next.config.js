6f91ae749e626348c87b2fc064bd16cffc2f9b64
Richard Chu <richardchu10@gmail.com>
2022-08-27 14:06:17 -0400

    Fix build when sentry variables are not provided

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 const withPWA = require('next-pwa');
 
+const isSentryEnabled =
+  process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
@@ -24,29 +26,32 @@ const SentryWebpackPluginOptions = {
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-  return withPWA(
-    withSentryConfig(
-      withBundleAnalyzer({
-        experimental: { esmExternals: true },
-        trailingSlash: true,
-        pwa: {
-          dest: 'public',
-          scope: '/app/',
-          disable: process.env.NODE_ENV === 'development',
-          dynamicStartUrlRedirect: '/login',
-          reloadOnOnline: false,
-          register: false,
-          skipWaiting: false,
-        },
-        i18n: {
-          locales: ['en-US'],
-          defaultLocale: 'en-US',
-        },
-        env: {
-          BASE_URL: isDev ? 'http://localhost:3000' : 'https://notabase.io',
-        },
-      }),
-      SentryWebpackPluginOptions
-    )
+  let config = withPWA(
+    withBundleAnalyzer({
+      experimental: { esmExternals: true },
+      trailingSlash: true,
+      pwa: {
+        dest: 'public',
+        scope: '/app/',
+        disable: process.env.NODE_ENV === 'development',
+        dynamicStartUrlRedirect: '/login',
+        reloadOnOnline: false,
+        register: false,
+        skipWaiting: false,
+      },
+      i18n: {
+        locales: ['en-US'],
+        defaultLocale: 'en-US',
+      },
+      env: {
+        BASE_URL: isDev ? 'http://localhost:3000' : 'https://notabase.io',
+      },
+    })
   );
+
+  if (isSentryEnabled) {
+    config = withSentryConfig(config, SentryWebpackPluginOptions);
+  }
+
+  return config;
 };
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,13 +6,14 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0,
-  enabled: process.env.NODE_ENV === 'production',
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-});
+SENTRY_DSN &&
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 0,
+    enabled: process.env.NODE_ENV === 'production',
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+  });
