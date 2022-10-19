45e6cfc8f4d7fb8106f863f7df412942022f777c
Richard Chu <churichard@users.noreply.github.com>
2022-10-19 12:43:35 -0700

    Update slate-react to 0.67, treat value as initial value (#1200)

--- editor/transforms.ts
+++ editor/transforms.ts
@@ -1,5 +1,4 @@
-import type { Editor, Path } from 'slate';
-import { Transforms } from 'slate';
+import { Editor, Location, Node, Path, Point, Transforms } from 'slate';
 
 // Deletes `length` characters at the specified path and offset
 export const deleteText = (
@@ -17,4 +16,43 @@ export const deleteText = (
     focus: { path, offset },
   };
   Transforms.delete(editor, { at: range });
+};
+
+/**
+ * resetNodes resets the value of the editor.
+ * It should be noted that passing the `at` parameter may cause a "Cannot resolve a DOM point from Slate point" error.
+ */
+export const resetNodes = (
+  editor: Editor,
+  options: {
+    nodes?: Node | Node[];
+    at?: Location;
+  } = {}
+): void => {
+  const children = [...editor.children];
+
+  children.forEach((node) =>
+    editor.apply({ type: 'remove_node', path: [0], node })
+  );
+
+  if (options.nodes) {
+    const nodes = Node.isNode(options.nodes) ? [options.nodes] : options.nodes;
+
+    nodes.forEach((node, i) =>
+      editor.apply({ type: 'insert_node', path: [i], node: node })
+    );
+  }
+
+  const point =
+    options.at && Point.isPoint(options.at)
+      ? options.at
+      : Editor.end(editor, []);
+
+  if (point) {
+    try {
+      Transforms.select(editor, point);
+    } catch (e) {
+      // Do nothing. Selection was not able to be restored.
+    }
+  }
 };
