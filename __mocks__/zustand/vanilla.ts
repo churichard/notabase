e6d45747054b7a5e504114f2f90367bae20ea00f
Richard Chu <richardchu10@gmail.com>
2023-01-11 22:49:26 -0500

    Fix tests by updating zustand mock

--- __mocks__/zustand/vanilla.ts
+++ __mocks__/zustand/vanilla.ts
@@ -1,14 +1,14 @@
-import actualCreate, { StateCreator } from 'zustand/vanilla';
+import { createStore as actualCreate, StateCreator } from 'zustand/vanilla';
 import { act } from '@testing-library/react';
 
 // a variable to hold reset functions for all stores declared in the app
 const storeResetFns = new Set<() => void>();
 
 // when creating a store, we get its initial state, create a reset function and add it in the set
-const create =
+const createStore =
   <T>() =>
   (createState: StateCreator<T>) => {
-    const store = actualCreate(createState);
+    const store = actualCreate<T>(createState);
     const initialState = store.getState();
     storeResetFns.add(() => store.setState(initialState, true));
     return store;
@@ -19,4 +19,4 @@ afterEach(() => {
   act(() => storeResetFns.forEach((resetFn) => resetFn()));
 });
 
-export default create;
+export { createStore };
