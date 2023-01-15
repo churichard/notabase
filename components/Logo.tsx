bb701ee12f6d1ff862ef3285133dcbc63e69b8d8
Richard Chu <richardchu10@gmail.com>
2023-01-15 11:58:05 -0500

    Migrate to next/image

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/legacy/image';
+import Image from 'next/image';
 import logo from 'public/logo.svg';
 
 type Props = {
@@ -8,13 +8,5 @@ type Props = {
 
 export default function Logo(props: Props) {
   const { width, height } = props;
-  return (
-    <Image
-      src={logo}
-      width={width}
-      height={height}
-      alt="Notabase logo"
-      layout="fixed"
-    />
-  );
+  return <Image src={logo} width={width} height={height} alt="Notabase logo" />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/legacy/image';
+import Image from 'next/image';
 import {
   IconArrowRight,
   IconBrandGithub,
