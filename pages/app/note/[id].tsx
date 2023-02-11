bc797492ab3ce402256ba23ab89dbb3978470096
Richard Chu <richardchu10@gmail.com>
2023-02-11 17:17:37 -0500

    Refactor getHighlightedPath into separate file

--- pages/app/note/[id].tsx
+++ pages/app/note/[id].tsx
@@ -8,6 +8,7 @@ import { useStore } from 'lib/store';
 import usePrevious from 'utils/usePrevious';
 import { queryParamToArray } from 'utils/url';
 import useBlockBacklinks from 'editor/backlinks/useBlockBacklinks';
+import getHighlightedPath from 'utils/getHighlightedPath';
 
 export default function NotePage() {
   const router = useRouter();
@@ -112,32 +113,3 @@ export default function NotePage() {
     </>
   );
 }
-
-/**
- * Takes in a url with a hash parameter formatted like #1-2,3 (where 1 signifies the open note index,
- * and 2,3 signifies the path to be highlighted). Parses the url and
- * returns the open note index and the path to be highlighted as an object.
- */
-const getHighlightedPath = (
-  url: string
-): { index: number; path: Path } | null => {
-  const urlArr = url.split('#');
-  if (urlArr.length <= 1) {
-    return null;
-  }
-
-  const hash = urlArr[urlArr.length - 1];
-  const [strIndex, ...strPath] = hash.split(/[-,]+/);
-
-  const index = Number.parseInt(strIndex);
-  const path = strPath.map((pathSegment) => Number.parseInt(pathSegment));
-  if (
-    Number.isNaN(index) ||
-    path.length <= 0 ||
-    path.some((segment) => Number.isNaN(segment))
-  ) {
-    return null;
-  }
-
-  return { index, path };
-};
